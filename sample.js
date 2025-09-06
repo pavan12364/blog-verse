38a3ac364a77e1bc165fcf3d0add9478760952c4
unknown <bankurupavansai2007@gmail.com>
2025-09-06 14:14:11 +0530

    lecture of prem sagar

--- sample.js
+++ sample.js
@@ -1,1 +1,24 @@
 console.log("start learning java script");
+let name = "Dev Astra";
+console.log((name))
+let num=07
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof true)
+
+let x
+console.log(typeof x)
+
+let y=null;
+console.log(y)
+console.log(typeof y)
+
+let arr=[1,3,5,7,9]
+console.log(arr[5])
+
+let person ={name:"deva",age:18,branch:"cse"}
+console.log(person.name)
+console.log(person.age)
+console.log(person.branch)
+console.log(typeof person)
