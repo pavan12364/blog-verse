ddff18a71f8e8fe0515a65c002e2c0d9fb6906ee
unknown <bankurupavansai2007@gmail.com>
2025-09-06 15:56:52 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,24 @@
+function greet(){
+    console.log("Welcome To My Devastra")
+}
+greet();
+
+function add(x,y){
+    return x+y;
+}
+let result=add(7,14)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply=function(x,y){
+  return x*y;
+}
+ console.log(multiply(5,3))
+ const square =(x)=>x*x;
+ console.log(square(5))
+
+ const subsract=(x,y)=>x-y;
+ console.log(subsract(20,25))
+ 
