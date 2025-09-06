f4a81ae989934bba2916f484ade2d52468e208da
unknown <bankurupavansai2007@gmail.com>
2025-09-06 17:13:26 +0530

    .

---
+++ asyncArray.js
@@ -0,0 +1,9 @@
+const sampleData=async()=>{
+    try{
+        let response=await fetch("https:www.goggle.com");
+        console.log(response)
+    }catch(err){
+        console.log("error accessing google:"+err)
+    }
+}
+sampleData()
